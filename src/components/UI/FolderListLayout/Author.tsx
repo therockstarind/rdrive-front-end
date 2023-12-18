9245d5172eaa8316c00946e838ad6f14333bb21b
Rock Star <rohitkumarrohit96@gmail.com>
2023-12-18 18:41:30 +0530

    Update author component styles.

--- src/components/UI/FolderListLayout/Author.tsx
+++ src/components/UI/FolderListLayout/Author.tsx
@@ -7,16 +7,16 @@ return (
     <Flex gap="3" align="center">
       <Avatar size="3" src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop" fallback="T" />
       <Box>
-        <Text as="div" size="2" weight="bold">
+        <Text as="div" size="3" weight="bold">
           Teodros Girmay
         </Text>
-        <Text as="div" size="2" color="gray">
+        <Text as="p" size="1" color="gray">
           Software & Hardware
         </Text>
       </Box>
     </Flex>
     <Flex>
-      <Text as="div" size="2" color="gray">
+      <Text as="p" size="1" color="gray">
         8 hours ago
       </Text>
     </Flex>
