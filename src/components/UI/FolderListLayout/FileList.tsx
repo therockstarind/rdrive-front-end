05464ed64a7906de5ef37a2ccf8c23ff874e7e65
Rock Star <rohitkumarrohit96@gmail.com>
2023-12-19 11:19:09 +0530

    Update FileList component styles

--- src/components/UI/FolderListLayout/FileList.tsx
+++ src/components/UI/FolderListLayout/FileList.tsx
@@ -8,7 +8,7 @@ import { RiDownloadLine } from "react-icons/ri";
 
 const FileList = () => {
 return (
-        <Flex justify="between" align="center">
+        <Flex justify="between" align="center" mr="2">
           <Flex gap="2" align="center"><Box><BsImage /></Box><Text size="2" className="line-clamp-1">ice eea global images V13 0 3 0 SGMEUXM 20220915 0000 00 12 0 dc6cb84563.zip</Text></Flex>
           <Flex className="hidden md:flex" align="center"><Text size="1">24 Dec 2022</Text></Flex>
           <Flex className="hidden md:flex" align="center"><Text size="1">1.89 GB</Text></Flex>
