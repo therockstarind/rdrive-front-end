749bb54852410b6465303b4e56223ddb5425c8ab
Rock Star <rohitkumarrohit96@gmail.com>
2023-12-26 11:11:13 +0530

    Update About, Privacy, and Terms pages

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { Flex, Heading, Strong, Text } from "@radix-ui/themes";
+import Link from "next/link";
+import AnimatedHeading from "®/components/FramerMotion/AnimatedHeading";
+import { fromLeftVariant } from "®/lib/FramerMotionVariants";
+
 export default function Terms() {
   return (
-    <main>
-      
-    </main>
+    <Flex direction="column" className="mx-auto max-w-6xl" p={{initial: '2', sm: '0'}}>
+    <AnimatedHeading variants={fromLeftVariant}>
+      <Heading as="h1" size="8" className="mb-4" align={{initial: 'left', md: 'center'}}>Privacy Policy</Heading>
+    </AnimatedHeading>
+  </Flex>
   )
 }
--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { Flex, Heading, Link } from "@radix-ui/themes"
+import AnimatedHeading from "®/components/FramerMotion/AnimatedHeading"
+import { fromLeftVariant } from "®/lib/FramerMotionVariants"
+
 export default function PrivacyPolicy() {
   return (
-    <main>
-      
-    </main>
+    <Flex direction="column" className="mx-auto max-w-6xl" p={{initial: '2', sm: '0'}}>
+    <AnimatedHeading variants={fromLeftVariant}>
+      <Heading as="h1" size="8" className="mb-4" align={{initial: 'left', md: 'center'}}>Terms and rules</Heading>
+    </AnimatedHeading>
+    </Flex>
   )
 }
